1d377bfb1c32061ddc0f013bbda9276d48f5ea6f
Yana Kolomytseva <y.kolomytseva@gmail.com>
2023-10-16 12:24:44 +0200

    all_tasks_done

--- src/App.js
+++ src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle/>;
+}
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello World Article</h1>
+      <p>This is a paragraph</p>
+    </article>
+  );
+
 }
